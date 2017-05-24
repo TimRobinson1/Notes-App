5967b872b451921abfcc2b6010f69679442958df
Timothy Robinson <timmyrobby@gmail.com>
2017-05-24 12:34:16 +0100

    adds notePad creator and addNote

--- src/notePad.js
+++ src/notePad.js
@@ -1,3 +1,11 @@
-function shortenString(string) {
+function NotePad() {
+  this.notes = [];
+}
+
+NotePad.prototype.addNote = function(note) {
+  this.notes.push(note);
+}
+
+NotePad.prototype.shortenString = function(string) {
   return string.substr(0, 20);
 }
--- tests/notePadSpec.js
+++ tests/notePadSpec.js
@@ -1,7 +1,21 @@
 describe('Note Pad', function() {
+  var notePad = new NotePad();
+
+  it('is instantiated with a empty note array', function() {
+    expect(notePad.notes.length).toEqual(0);
+  })
+
   describe('#shortenString', function() {
     it('shortens a string to its first 20 chars', function() {
-      expect(shortenString('Tim is a really nice team mate. But which Tim do I mean?')).toEqual('Tim is a really nice');
+      expect(notePad.shortenString('Tim is a really nice team mate. But which Tim do I mean?')).toEqual('Tim is a really nice');
     })
   })
+
+  describe('#addNote', function() {
+    it('adds a note to the notes array', function() {
+      notePad.addNote('Nigels test note');
+      expect(notePad.notes[0]).toEqual('Nigels test note');
+    })
+  })
+
 })
