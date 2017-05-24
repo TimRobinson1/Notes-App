793c16236f495e190c7ecd4383329d178b96412a
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2017-05-24 15:56:31 +0100

    adds shortening char functinality

--- tests/featuresSpec.js
+++ tests/featuresSpec.js
@@ -9,4 +9,10 @@ describe('Fill in form', function() {
   it('adds the note to the page', function() {
     expect("notes").toHaveContent('This is a test note');
   })
+
+  it('shortens a note longer than 20 chars', function() {
+    fillIn('note').with('This is really quite a long test note');
+    click('create');
+    expect("notes").toHaveContent('This is really quite');
+  })
 })
