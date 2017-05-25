64ce2102a0af800fb53cd45bb50da661ff3bd771
Timothy Robinson <timmyrobby@gmail.com>
2017-05-25 17:47:35 +0100

    Cleans up interface by removing redundant loop

--- src/interface.js
+++ src/interface.js
@@ -1,21 +1,15 @@
 var notePad = new NotePad();
+var note_counter = 0;
 
 document.getElementById("create").addEventListener("click", function() {
   notePad.addNote(document.getElementById("note").value);
+  var note = notePad.shortenString(notePad.notes[note_counter]),
+      list = document.getElementById("notes");
+  list.innerHTML = `<a href='#${note_counter}'><p id='entry${note_counter}'>${note}</p></a>` + list.innerHTML;
+  note_counter++;
+});
 
-  var note_counter = 0
-  document.getElementById("notes").innerHTML = "";
-
-  notePad.notes.forEach(function(note) {
-      note = notePad.shortenString(note);
-      var list = document.getElementById("notes")
-      list.innerHTML = `<a  href='#${note_counter}'><p id='entry${note_counter}'>${note}</p></a>` + list.innerHTML;
-      note_counter++
-    })
-  })
-
-  window.addEventListener("hashchange", function() {
-    var indexNumber = window.location.hash.split("#")[1];
-    document.getElementById("note-display")
-    .innerHTML = notePad.notes[indexNumber]
-  });
+window.addEventListener("hashchange", function() {
+  var index = window.location.hash.split("#")[1];
+  document.getElementById("note-display").innerHTML = notePad.notes[index];
+});
