38e636dc6b6e96a4d6acc38e0668fff945435ada
Unai Motriko <unaimotriko@gmail.com>
2017-05-26 10:43:55 +0100

    It adds delete functionality

--- src/notePad.js
+++ src/notePad.js
@@ -9,3 +9,11 @@ NotePad.prototype.addNote = function(note) {
 NotePad.prototype.shortenString = function(string) {
   return string.substr(0, 20);
 }
+
+NotePad.prototype.deleteNote = function (string) {
+  this.notes = this.notes.slice(this.notes.indexOf(string))
+};
+
+NotePad.prototype.newIndex = function (string) {
+  return this.notes.indexOf(string)
+};
